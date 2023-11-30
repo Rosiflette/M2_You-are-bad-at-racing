0b4cba9c21a1da44f44690f8f9acc71e0efb4dd5
Rose <janinejacques.tod@gmail.com>
2023-11-30 14:34:37 +0100

    NEW : Orientation car

---
+++ Assets/Scripts/MovingCarMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCarMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x-450, gameObject.transform.position.y -300, -1000);
+        gameObject.transform.LookAt(mousePosition);
+    }
+}
