0faa679655d21b144707e0dee973289c0b29795e
Rose <janinejacques.trod@gmail.com>
2023-12-05 14:32:11 +0100

    NEW : Add timer to level interface

---
+++ Assets/Scripts/Level_UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Level_UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textTimer;
+    private float timePassed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timePassed/60);
+        int seconds = Mathf.FloorToInt(timePassed % 60);
+        textTimer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
