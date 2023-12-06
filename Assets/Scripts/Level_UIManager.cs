2fa9a73e9afb6c002855cd4cad0b81573ee762b1
Rose <janinejacques.trod@gmail.com>
2023-12-06 11:43:26 +0100

    NEW : Add pause play button

--- Assets/Scripts/Level_UIManager.cs
+++ Assets/Scripts/Level_UIManager.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
+
 
 public class Level_UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textTimer;
+
+    [SerializeField] private Sprite pauseImage;
+    [SerializeField] private Sprite playImage;
+
     private float timePassed = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         timePassed += Time.deltaTime;
-        int minutes = Mathf.FloorToInt(timePassed/60);
+        int minutes = Mathf.FloorToInt(timePassed / 60);
         int seconds = Mathf.FloorToInt(timePassed % 60);
         int milliseconds = Mathf.FloorToInt((timePassed - Mathf.FloorToInt(timePassed)) * 100);
-        textTimer.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds,milliseconds);
+        textTimer.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+    }
+
+    public void PauseGame(GameObject button)
+    {
+        if(Time.timeScale == 0){
+            Time.timeScale = 1;
+            button.GetComponent<Image>().sprite = pauseImage;
+        }
+        else{
+            button.GetComponent<Image>().sprite = playImage;
+            Time.timeScale = 0;
+        }
     }
 }
