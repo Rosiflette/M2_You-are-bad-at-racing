7cfc67cf5ff70476847e157de82fcd4bb9b4112f
yedenn <ianis.olivier.pacaud@gmail.com>
2023-12-07 15:52:41 +0100

    REF : PHYSIQUE DE MERDE
    C'est vraiment pas ce que je veux :)

--- Assets/Scripts/PlayerTestBALL.cs
+++ Assets/Scripts/PlayerTestBALL.cs
@@ -8,11 +8,13 @@ public class PlayerTestBALL : MonoBehaviour
     [SerializeField] float maxSpeed;
 
     Rigidbody rig;
+    float radius;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody>();
+        radius = GetComponent<SphereCollider>().radius;
     }
 
     private void OnDrawGizmos()
@@ -55,19 +57,28 @@ public class PlayerTestBALL : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool grounded = false;
+        if (Physics.Raycast(transform.position, Vector3.down, radius))
+            grounded = true;
+
+
         float steer = GameManager.Instance.InputController.SteerInput;
         float throttle = GameManager.Instance.InputController.ThrottleInput;
 
-        float force = 10f;
+        float force = 5f;
 
-        car.Rotate(Vector3.up * steer * 1.5f);
+        float angle = Vector3.Angle(car.forward, rig.velocity);
+        if (angle < 45 || !grounded)
+        {
+            car.Rotate(Vector3.up * steer * 1.25f);
+        }
         if (steer != 0)
         {
             force *= 2;
         }
 
 
-        if (throttle == 0 && (rig.velocity - Vector3.up * rig.velocity.y).magnitude != 0)
+        if (false && throttle == 0 && (rig.velocity - Vector3.up * rig.velocity.y).magnitude != 0)
         {
             rig.AddForce(- rig.velocity, ForceMode.Force);
         }
@@ -81,55 +92,14 @@ public class PlayerTestBALL : MonoBehaviour
             }
         }
 
-        Debug.DrawRay(car.position, Vector3.down * 5, Color.red);
+        Debug.DrawRay(car.position, car.forward * 10, Color.red);
+        Debug.DrawRay(transform.position, rig.velocity.normalized * 10, Color.green);
 
-        RaycastHit hitCenter;
-
-        Physics.Raycast(car.position, Vector3.down, out hitCenter);
-
+        Physics.Raycast(car.position, Vector3.down, out RaycastHit hitCenter);
         Vector3 proj = car.forward - (Vector3.Dot(car.forward, hitCenter.normal)) * hitCenter.normal;
-
         Quaternion rotationRef = Quaternion.Lerp(car.rotation, Quaternion.LookRotation(proj, hitCenter.normal), Time.deltaTime * 10);
-
         car.rotation = rotationRef;
 
-
-        //if (Physics.Raycast(car.position, Vector3.down, out RaycastHit hit, 5f))
-        //{
-        //    //Physics.Raycast(car.position + car.forward, Vector3.down, out RaycastHit forwardHit, 8);
-        //    //Physics.Raycast(car.position - car.forward, Vector3.down, out RaycastHit backHit);
-
-        //    //Physics.Raycast(car.position + car.right, Vector3.down, out RaycastHit rightHit);
-        //    //Physics.Raycast(car.position - car.right, Vector3.down, out RaycastHit leftHit);
-
-        //    //Gizmos.DrawSphere(backHit.point, 0.2f);
-        //    //Gizmos.DrawSphere(forwardHit.point, 0.2f);
-        //    //Gizmos.DrawSphere(rightHit.point, 0.2f);
-        //    //Gizmos.DrawSphere(leftHit.point, 0.2f);
-
-        //    //Debug.DrawLine(backHit.point + Vector3.down, backHit.point + Vector3.up, Color.blue);
-        //    //Debug.DrawLine(forwardHit.point + Vector3.down, forwardHit.point + Vector3.up, Color.blue);
-        //    //Debug.DrawLine(rightHit.point + Vector3.down, rightHit.point + Vector3.up, Color.blue);
-        //    //Debug.DrawLine(leftHit.point + Vector3.down, leftHit.point + Vector3.up, Color.blue);
-
-        //    //float angleFrontBack = Vector3.SignedAngle(car.forward, forwardHit.point - backHit.point, car.right);
-        //    //print(angleFrontBack);
-        //    //float angleLeftRight = Vector3.SignedAngle(car.right, rightHit.point - leftHit.point, car.forward);
-        //    //angleLeftRight = car.rotation.eulerAngles.z;
-
-        //    //car.rotation = Quaternion.Euler(new Vector3(angleFrontBack, car.rotation.eulerAngles.y, angleLeftRight));
-        //    //car.Rotate(car.right, angleFrontBack);
-        //    //car.Rotate(car.forward, angleLeftRight);
-
-        //    if (Vector3.Cross(hit.normal, car.up) != Vector3.zero)
-        //    {
-        //        print("rotate");
-        //        car.rotation = Quaternion.FromToRotation(car.up, hit.normal);
-        //    }
-
-        //}
-
-
-        car.position = transform.position;
+        car.position = transform.position + Vector3.down * (radius - 0.1f);
     }
 }
